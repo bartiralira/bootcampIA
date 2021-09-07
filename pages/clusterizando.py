ab50ba36da5853a91d8552f7aba45d1630cc7808
bartiralira <lira.bartira@gmail.com>
2021-09-07 18:39:37 -0300

    7

--- pages/clusterizando.py
+++ pages/clusterizando.py
@@ -24,12 +24,17 @@ from multipage import MultiPage
 # deixa a pagina full wide
 #st.set_page_config(layout="wide")
 def app():
-    st.markdown("## Clusterizando")
+    #st.markdown("## Clusterizando")
 
     # preparando a sepracao da pagina
     header = st.container()
-    dataset = st.container()
-    app = MultiPage()
+    overview = st.container()
+    cluster0 = st.container()
+    cluster1 = st.container()
+    cluster2 = st.container()
+    cluster3 = st.container()
+    cluster4 = st.container()
+    #app = MultiPage()
     modelo = joblib.load("./modelo/classificador_cliente.pkl")
 
 
@@ -41,62 +46,151 @@ def app():
         file.drop(file.columns[0],axis=1,inplace=True)
         return file
 
-
-    # sidebar
-    #st.sidebar.title("BootcampIA - Turma 17IA")
-    #st.sidebar.markdown("Equipe: Bartira, Vitor, Tiago e Ricardo")
-
-    #faz as escolhas das paginas
-    #radioButton = st.sidebar.radio("Go to",['Home', 'Novo Cliente'])
-    #if (radioButton == "Novo Cliente"):
-    #    app.add_page("Novo Cliente", new_customer.app)
-
     with header:
-        st.title("Criando clusters:")   
-        
-    with dataset:
-        st.subheader("Clusters gerados")
+        st.title("Clusterizando:")   
+
         df = get_data('./data/datasetclusterizado.csv')
 
         cluster=pd.to_numeric(df.cluster.unique())
-        #df["Permanência Origem (h)"] = df["Permanência Origem (h)"].astype(int)
-        #pd.to_numeric(df["Permanência Origem (h)"])
-        #df["Permanência no Destino (h)"] = pd.to_numeric(df["Permanência no Destino (h)"])
-        #selecao = sel_col.selectbox("Select",origem[["Local TD"]], index=0)
         cluster_min=int(cluster.min())
         cluster_max=int(cluster.max())
         sel_col= st.columns(1)
 
-        selecao = st.slider("Select",min_value=cluster_min, max_value=cluster_max)
+        
 
-    with dataset:
+    with overview:
 
         X_minmax = MinMaxScaler().fit_transform(df.values)
         df_norm = pd.DataFrame(X_minmax)
         df_norm.columns = df.columns
         df_norm['cluster'] = modelo.labels_ #bestKmeans.labels_
         #df_norm.head()  
-        df_res = df_norm.groupby('cluster',as_index=False).mean()
         
-        #plt.figure(1, figsize=(32, 10))
-        #fig, axes = plt.subplots(1, figsize=(32,10))
-        fig = px.bar(df_res, x="cluster", y=df_res.columns, title="Wide-Form Input")
+        # seletor
+        selecao = st.multiselect("Selecione a variável",df_norm.columns)
+        #titulo
+        st.subheader("Visão gerão da divisão dos cluster. Temos 5 cluster identificados com caracteristicas distintas")
+        
+        # agrupando os cluster para plotar no grafico de barras
+        df_res = df_norm.groupby('cluster',as_index=False).mean()
+        #df_res.columns
+        if (selecao):
+            fig = px.bar(df_res, x="cluster", y=selecao, title="Wide-Form Input")
+        else:
+            fig = px.bar(df_res, x="cluster", y=df_res.columns, title="Wide-Form Input")
         fig.update_layout(margin=dict(l=0, r=0, b=0, t=0),autosize=False,height=400,width=1400)
         st.write(fig)
 
 
 
-    with dataset:
-        labels = modelo.labels_
-        qtdClusters = len(set(labels))
+    with cluster0:
 
-        plt.figure(figsize=(10,10))
-        fig, axes = plt.subplots(nrows=qtdClusters, ncols=1, figsize=(20,15))
+        st.subheader("Cluster 0:")
+        st.markdown("**Caracteristica:** Grande empresas com alto score de pontualidade e uma consideravel margem bruta acumulada. ")
+        st.write("")
+        st.write("")
+        col1, col2 = st.columns([1,1.2])
+# grafico de barra
+        medias =  df_norm[ df_norm.cluster== 0].mean()
+        medias.drop(['cluster'], inplace = True)
 
-        for c in set(labels):        
-            medias =  df_norm[ df_norm.cluster== c].mean()
-            medias.drop(['cluster'], inplace = True)
-            medias.plot(ax = axes[c], kind='bar', fontsize=8, title = 'Cluster: ' + str(c), rot=15 )
-        
-        fig.tight_layout()
-        st.write(fig)
+        df_medias=pd.DataFrame(medias)
+        #
+        fig0 = px.bar(df_medias, x=df_medias.columns, title="Cluster 0")
+        fig0.update_layout(margin=dict(l=0, r=0, b=0, t=0),autosize=True)
+        col1.write(fig0)
+# grafico de radar
+        colnames = df_norm.drop(['cluster'], axis = 1).columns
+
+        fig00 = px.line_polar(medias,r=medias.values , theta= colnames, line_close=True)
+        fig00.update_traces(fill='toself')
+        col2.write(fig00)
+
+    with cluster1:
+
+        st.subheader("Cluster 1:")
+        st.markdown("**Caracteristica:** Apenas empresas pequenas empresas com score de pontualidade alto.")
+        st.write("")
+        st.write("")
+        col1, col2 = st.columns([1,1.2])
+# grafico de barra
+        medias =  df_norm[ df_norm.cluster== 1].mean()
+        medias.drop(['cluster'], inplace = True)
+
+        df_medias=pd.DataFrame(medias)
+
+        fig0 = px.bar(df_medias, x=df_medias.columns, title="Cluster 1")
+        fig0.update_layout(margin=dict(l=0, r=0, b=0, t=0),autosize=True)
+        col1.write(fig0)
+# grafico de radar
+        colnames = df_norm.drop(['cluster'], axis = 1).columns
+        fig00 = px.line_polar(medias,r=medias.values , theta= colnames, line_close=True)
+        fig00.update_traces(fill='toself')
+        col2.write(fig00)
+
+    with cluster2:
+
+        st.subheader("Cluster 2:")
+        st.markdown("**Caracteristica:** Apenas pequenas empresas com baixo score de pontualidade e com algumas restrições.")
+        st.write("")
+        st.write("")
+        col1, col2 = st.columns([1,1.2])
+# grafico de barra
+        medias =  df_norm[ df_norm.cluster== 2].mean()
+        medias.drop(['cluster'], inplace = True)
+
+        df_medias=pd.DataFrame(medias)
+
+        fig0 = px.bar(df_medias, x=df_medias.columns, title="Cluster 2")
+        fig0.update_layout(margin=dict(l=0, r=0, b=0, t=0),autosize=True)
+        col1.write(fig0)
+# grafico de radar
+        colnames = df_norm.drop(['cluster'], axis = 1).columns
+        fig00 = px.line_polar(medias,r=medias.values , theta= colnames, line_close=True)
+        fig00.update_traces(fill='toself')
+        col2.write(fig00)
+
+    with cluster3:
+
+        st.subheader("Cluster 3:")
+        st.markdown("**Caracteristica:** Grandes empresas com baixo score de pontualidade e indice de dashboard alto e com definição de risco médio.")
+        st.write("")
+        st.write("")
+        col1, col2 = st.columns([1,1.2])
+# grafico de barra
+        medias =  df_norm[ df_norm.cluster== 3].mean()
+        medias.drop(['cluster'], inplace = True)
+
+        df_medias=pd.DataFrame(medias)
+
+        fig0 = px.bar(df_medias, x=df_medias.columns, title="Cluster 3")
+        fig0.update_layout(margin=dict(l=0, r=0, b=0, t=0),autosize=True)
+        col1.write(fig0)
+# grafico de radar
+        colnames = df_norm.drop(['cluster'], axis = 1).columns
+        fig00 = px.line_polar(medias,r=medias.values , theta= colnames, line_close=True)
+        fig00.update_traces(fill='toself')
+        col2.write(fig00)
+
+
+    with cluster4:
+
+        st.subheader("Cluster 4:")
+        st.markdown("**Caracteristica:** Clientes com restrições e com alto score de pontualidade.")
+        st.write("")
+        st.write("")
+        col1, col2 = st.columns([1,1.2])
+# grafico de barra
+        medias =  df_norm[ df_norm.cluster== 4].mean()
+        medias.drop(['cluster'], inplace = True)
+
+        df_medias=pd.DataFrame(medias)
+
+        fig0 = px.bar(df_medias, x=df_medias.columns, title="Cluster 4")
+        fig0.update_layout(margin=dict(l=0, r=0, b=0, t=0),autosize=True)
+        col1.write(fig0)
+# grafico de radar
+        colnames = df_norm.drop(['cluster'], axis = 1).columns
+        fig00 = px.line_polar(medias,r=medias.values , theta= colnames, line_close=True)
+        fig00.update_traces(fill='toself')
+        col2.write(fig00)
