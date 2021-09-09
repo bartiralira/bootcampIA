cf7c1eb91ce21e6dbba300afa2c27ce650f28167
bartiralira <lira.bartira@gmail.com>
2021-09-09 13:57:52 -0300

    11

--- pages/new_customer.py
+++ pages/new_customer.py
@@ -48,35 +48,39 @@ def app():
         st.title("Adicionando novo cliente:")
         df = get_data('./data/datasetclusterizado.csv')   
     
-    with st.form(key='my_form'):
-        ativoCirculante = st.number_input("Ativo Circulante", format="%.2f")
-        capitalSocial = st.number_input('Capital Social', format="%.2f")
-        custos = st.number_input('Custos', format="%.2f")
-        dashboardCorrelacao=st.slider('Dashboard Correlação',-10.0,10.0,0.0)
-        definicaoRisco=st.slider('Definição de risco',0, 4,0)
-        empresa_MeEppMei=st.radio("Empresa ME/MEI/EPP",("Sim","Não"))
-        endividamento = st.number_input('Endividamento', format="%.2f")
-        estoque = st.number_input('Estoque', format="%.2f")
-        faturamentoBruto = st.number_input('Faturamento Bruto', format="%.2f")
-        limiteEmpresaAnaliseCredito = st.number_input('Analise de Crédito', format="%.2f")
-        maiorAtraso=st.slider('Mario atraso(dias)',0, 1000)
-        margemBruta = st.number_input('Margem bruta', format="%.2f")
-        margemBrutaAcumulada = st.number_input('Margem bruta acumulada', format="%.2f")
-        passivoCirculante = st.number_input('Passivo circulante', format="%.2f")
-        percentualProtestos=st.slider('Percentual de protestos (%)',0, 100)
-        prazoMedioRecebimentoVendas=st.slider('Prazo médio de recebimento de vendas',0, 1000)
-        restricoes=st.radio('Restrições',("Sim","Não"))
-        scorePontualidade=st.slider('Score de pontualidade',0.0, 1.0)
-        titulosEmAberto = st.number_input('Títulos em aberto', format="%.2f")
-        totalPatrimonioLiquido = st.number_input('Patrimonio líquido', format="%.2f")
-        valorAprovado = st.number_input('Valor aprovado', format="%.2f")
-        valorSolicitado = st.number_input('Valor solicitado', format="%.2f")
-        submit_button = st.form_submit_button(label='Cluster Indicado')
+    with novo_cluster: 
+        form = st.form(key='my_form')
+        ativoCirculante = form.number_input("Ativo Circulante", format="%.2f")
+        capitalSocial = form.number_input('Capital Social', format="%.2f")
+        custos = form.number_input('Custos', format="%.2f")
+        dashboardCorrelacao=form.slider('Dashboard Correlação',-10.0,10.0,0.0)
+        definicaoRisco=form.slider('Definição de risco',0, 4,0)
+        empresa_MeEppMei= (1 if form.radio("Empresa ME/MEI/EPP",("Sim","Não"))=="Sim" else 0)
+        endividamento = form.number_input('Endividamento', format="%.2f")
+        estoque = form.number_input('Estoque', format="%.2f")
+        faturamentoBruto = form.number_input('Faturamento Bruto', format="%.2f")
+        limiteEmpresaAnaliseCredito = form.number_input('Analise de Crédito', format="%.2f")
+        maiorAtraso=form.slider('Mario atraso(dias)',0, 1000)
+        margemBruta = form.number_input('Margem bruta', format="%.2f")
+        margemBrutaAcumulada = form.number_input('Margem bruta acumulada', format="%.2f")
+        passivoCirculante = form.number_input('Passivo circulante', format="%.2f")
+        percentualProtestos=form.slider('Percentual de protestos (%)',0, 100)
+        prazoMedioRecebimentoVendas=form.slider('Prazo médio de recebimento de vendas',0, 1000)
+        restricoes=(1 if form.radio('Restrições',("Sim","Não")) == "Sim" else 0)
+        scorePontualidade=form.slider('Score de pontualidade',0.0, 1.0)
+        titulosEmAberto = form.number_input('Títulos em aberto', format="%.2f")
+        totalPatrimonioLiquido = form.number_input('Patrimonio líquido', format="%.2f")
+        valorAprovado = form.number_input('Valor aprovado', format="%.2f")
+        valorSolicitado = form.number_input('Valor solicitado', format="%.2f")
+        submit_button = form.form_submit_button(label='Cluster Indicado')
 
     with novo_cluster:
         if submit_button:
-            #dd=[[ativoCirculante,capitalSocial,custos,dashboardCorrelacao,definicaoRisco,empresa_MeEppMei,endividamento,#estoque,faturamentoBruto,limiteEmpresaAnaliseCredito,maiorAtraso,margemBruta,margemBrutaAcumulada,#passivoCirculante,percentualProtestos,prazoMedioRecebimentoVendas,restricoes,scorePontualidade,titulosEmAberto,#totalPatrimonioLiquido,valorAprovado,valorSolicitado]]
-            #df_new=pd.DataFrame(dd)
-            #df_new.columns = df.columns
-            st.write(restricoes)
-            #st.write(modelo.predict(df_new))
+            dd=[[ativoCirculante,capitalSocial,custos,dashboardCorrelacao,definicaoRisco,empresa_MeEppMei,endividamento,
+            estoque,faturamentoBruto,limiteEmpresaAnaliseCredito,maiorAtraso,margemBruta,margemBrutaAcumulada,
+            passivoCirculante,percentualProtestos,prazoMedioRecebimentoVendas,restricoes,scorePontualidade,titulosEmAberto,totalPatrimonioLiquido,valorAprovado,valorSolicitado]]
+            df_new=pd.DataFrame(dd)
+            df_new.columns = df.columns
+            cluster_indicado =modelo.predict(df_new)
+            st.write(f'O cliente é indicado para o cluster: {cluster_indicado}')
+            #st.write(empresa_MeEppMei)
