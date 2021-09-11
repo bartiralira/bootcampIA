e93e16c45794a93692744993a1dfb0974f3c674a
bartiralira <lira.bartira@gmail.com>
2021-09-11 15:17:10 -0300

    55

--- pages/previsao_semana.py
+++ pages/previsao_semana.py
@@ -5,12 +5,8 @@ import missingno as msno
 import seaborn as sbs
 import sklearn.metrics as sm
 import matplotlib.pyplot as plt
-from sklearn.cluster import KMeans
-import plotly.express as px
-from sklearn.preprocessing import StandardScaler, MinMaxScaler
-from sklearn.metrics import silhouette_score
 #from tqdm import tqdm
-from sklearn.decomposition import PCA
+from keras.models import load_model
 import joblib
 
 import plotly.io as pio
@@ -21,25 +17,39 @@ import streamlit as st
 from multipage import MultiPage
 
 # comeca os containers
-@st.cache
-def get_data(filename):
-    file= pd.read_csv(filename,sep="\t",decimal='.', engine='python')
-    file = file.reindex(sorted(file.columns), axis=1)
-    file.drop(file.columns[0],axis=1,inplace=True)
-    return file
-
-
 def app():
-    st.markdown("## Valor Aprovado")
+    #st.markdown("## Novo cliente")
     # deixa a pagina full wide
     #st.set_page_config(layout="wide")
-
+    #app = MultiPage()
 
     # preparando a sepracao da pagina
     header = st.container()
-    dataset = st.container()
-    app = MultiPage()
-    modelo = joblib.load("./modelo/classificador_cliente.pkl")
+    col1, col2 = st.columns(2)
+
+    model_dia = load_model("./modelo/modelodia.hdf5")
+    model_semana = load_model("./modelo/modelosemanal.hdf5")
+
 
     with header:
-        st.title("Previsão na Semana:")
+        st.title("Previsão de volume de crédito:")
+        st.text("")
+        st.text("")
+
+    with col1: 
+        form1 = st.form(key='my_form1')
+        qtd_dias=form1.slider('Quantidade de dias:',0, 30,0)
+        valor_dia_button = form1.form_submit_button(label='Previsão diária')
+        if valor_dia_button:
+            previsao_dia =model_dia.predict(qtd_dias)
+            #imprimindo valor sugerido na tela
+            st.write(f'Previsão diária: {previsao_dia}')
+    
+    with col2: 
+        form2 = st.form(key='my_form2')
+        qtd_semanas=form2.slider('Quantidade de semana:',0, 30,0)
+        valor_semana_button = form2.form_submit_button(label='Previsão semanal')
+        if valor_semana_button:
+            previsao_semana =model_semana.predict(qtd_semanas)
+            #imprimindo valor sugerido na tela
+            st.write(f'Previsão semanal: {previsao_semana}')
